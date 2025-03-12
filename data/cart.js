461530ec5959e40750fd05eb7d1f3b69e8f4c3c3
ROHIT0406 <rohitsir062003@gmail.com>
2025-03-12 12:45:23 +0530

    save cart into localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,13 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2
 },{
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity:1
 }];
-
+ function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addToCart(productId,selectedQuantity){
     let matchingItem;
     cart.forEach((item)=>{
@@ -20,7 +22,7 @@ export function addToCart(productId,selectedQuantity){
         productId : productId,
         quantity: selectedQuantity
     });}
-    
+    saveToStorage();
 }
 
 export function removeFromcart(productId){
@@ -31,4 +33,5 @@ export function removeFromcart(productId){
         }
     });
     cart = newCart;
+    saveToStorage();
 }
