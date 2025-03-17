d569aafa7e402b3cf08407464d62831a3da337f6
ROHIT0406 <rohitsir062003@gmail.com>
2025-03-17 16:27:40 +0530

    save data and generate html for delivery

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,14 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
+    {productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity:2,
+        deliveryOptionID:'1'
+    },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity:1,
+        deliveryOptionID:'2'
+    }
+
+];
 export function saveToStorage(){
     localStorage.setItem('cart',JSON.stringify(cart));
 }
@@ -14,7 +24,8 @@ export function addToCart(productId,selectedQuantity){
     }else{
     cart.push({
         productId : productId,
-        quantity: selectedQuantity
+        quantity: selectedQuantity,
+        deliveryOptionID: '1'
     });}
     saveToStorage();
 }
