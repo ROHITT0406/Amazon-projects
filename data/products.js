fb45150eeb5122a44419cfb85e001bd05e7dd2bb
ROHIT0406 <rohitsir062003@gmail.com>
2025-03-20 11:53:30 +0530

    convert loadProducts from xhr to fetch

--- data/products.js
+++ data/products.js
@@ -77,6 +77,29 @@ const object3 = {
 object3.method();*/
 //callback-a function to run in the future
 export let products = [];
+
+
+export function loadProductsFetch(){
+ const promise = fetch('https://supersimplebackend.dev/products').then((response)=>{
+   return response.json()
+  }).then((productsData) => {
+    products =productsData.map((productDetails)=>{
+      if (productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load products');
+
+  });
+  return promise;
+}
+
+/*
+loadProductsFetch().then(()=>{
+  console.log('next step');
+});
+
 export function loadProducts(fun){//callback
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('load',()=>{
@@ -93,7 +116,7 @@ export function loadProducts(fun){//callback
   xhr.send();
 }
 
-
+*/
 
 /*
 export const products = [
